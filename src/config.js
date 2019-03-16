3a3b9b18f799f261ca3f27fa1e6019fb201e96ab
lukasz_brainhub <lukasz@brainhub.pl>
2019-03-16 10:44:23 +0100

    Set default start date to 1 day ago

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 const config = {
   apiUrl: process.env.REACT_APP_API_URL,
   proxyApiUrl: process.env.REACT_APP_PROXY_API_URL || 'http://api.sensors.pluszczewski.pl',
-  defaultStartDateForCharts: moment().subtract(10, 'days'),
+  defaultStartDateForCharts: moment().subtract(1, 'days').startOf('day'),
 };
 
 export default config;
